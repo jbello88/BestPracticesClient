6e5f9a1cfcb292ed1c85ca6e4db5c45875e35c81
aa <aa@aa.com>
2020-08-24 17:44:46 +0200

    Code Cleanup

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const usersRoute = require("./routes/user.route");
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const connectWithDb = require("./db/connectWithDb");
+const cors = require("cors");
 
 (async () => {
+  if (!process.env.SECRET) {
+    console.error("FATAL ERROR: myprivatekey is not defined.");
+    process.exit(1);
+  }
 
-if (!process.env.SECRET) {
-  console.error("FATAL ERROR: myprivatekey is not defined.");
-  process.exit(1);
-}
+  await connectWithDb();
 
-await connectWithDb();
+  app.use(express.json());
+  app.use(cors());
 
-app.use(express.json());
+  app.use("/api/users", usersRoute);
 
-app.use("/api/users", usersRoute);
+  app.use((err, req, res, next) => {
+    console.error(new Date(), err.message);
+    res.send(
+      `Sorry! there was a problem with your last action: ${err.message}`
+    );
+  });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
 })();
